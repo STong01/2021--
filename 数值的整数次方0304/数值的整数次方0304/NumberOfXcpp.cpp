832d71946ec0666baaa74d4bcf39e1602270eac7
STong01 <1461638919@qq.com>
2021-03-08 14:28:23 +0800

    计算数值的n次方的值

---
+++ 数值的整数次方0304/数值的整数次方0304/NumberOfXcpp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//ÊµÏÖº¯Êýdouble Power
+double Power_1(double base, int exponent)
+{
+	double result = 1.0;
+	for (int i = 0; i <= exponent; i++)
+	{
+		result *= base;
+	}
+	return result;
+}
+
+
+double Power_2(double base, unsigned int exponent)
+{
+	if (exponent == 0)
+	{
+		return 1;
+	}
+	if (exponent == 1)
+	{
+		return base;
+	}
+
+	double result = Power_2(base, exponent >> 1);
+
+	result *= result;
+	if (exponent & 0x1 == 1)
+	{
+		result *= base;
+	}
+
+	return result;
+}
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
